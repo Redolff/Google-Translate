6cc7088b359d65e612cd9a94353fc0e40fa4e9bc
Federico Redolfo <redolfofederico@gmail.com>
2024-12-07 04:21:07 -0300

    integrate backEnd deploy

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
   useEffect(() => {
     if(debouncedFromText === '') return
     const sendMessageApi = async({ fromLanguage, toLanguage, debouncedFromText }: { fromLanguage: FromLanguage, toLanguage: Language, debouncedFromText: string }) => {
-      const API = 'http://localhost:3001/api/cohere';
+      const API = 'https://backendgoogletranslate.vercel.app/api/cohere';
       try{
         const response = await fetch(API, {
           method: 'POST',
